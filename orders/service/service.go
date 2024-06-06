f59ced683acd5ecf63153a10d08cc0bc3bfb3bde
millukii <mromerodev@gmail.com>
2024-06-05 22:29:32 -0400

    service mergeitems

--- orders/service/service.go
+++ orders/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"log"
 
 	pb "github.com/millukii/commons/api"
 	errors "github.com/millukii/commons/errors"
@@ -22,8 +23,42 @@ func (s *Service) CreateOrder(context.Context) error {
 }
 
 func (s *Service)	ValidateOrder(ctx context.Context, r pb.CreateOrderRequest) error{
-		if len(r.Items) ==0{
-			return errors.ErrNoItems
+	if (len(r.Items) ==0){
+		return errors.ErrNoItems
+	}
+
+	for _,i :=range r.Items{
+		if i.ID == ""{
+				return errors.ErrNoId
 		}
-		return nil
+		if i.Quantity <= 0{
+				return errors.ErrInvalidQuantity
+		}
+	}
+	mergedItems := mergeItemsQuantities(r.Items)
+
+	log.Println("merged items ", mergedItems)
+	//validate with stock service
+	return nil
+}
+
+func mergeItemsQuantities(items []*pb.ItemsWithQuantity) []*pb.ItemsWithQuantity{
+	merged := make([]*pb.ItemsWithQuantity,0)
+
+	for _, item := range items{
+		found := false
+
+		for _,finalItem := range merged {
+			if finalItem.ID == item.ID{
+				finalItem.Quantity += item.Quantity
+				found = true
+				break
+			}
+			if !found{
+				merged = append(merged, item)
+			}
+		}
+	}
+
+	return merged
 }
